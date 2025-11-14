022cdbc1a245ef464575b2024c42adb352b7bc13
iamrushier <rushikeshsurve193@gmail.com>
2025-11-14 11:02:51 +0530

    Data fetching logic added

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-
 const axios = require("axios");
 
 const app = express();
@@ -7,11 +6,33 @@ const PORT = process.env.PORT || 3000;
 
 async function fetchApiData(species) {
   const apiResponse = await axios.get(
-    `httpsL//www.fishwatch.gov/api/species/${species}`
+    `https://www.fishwatch.gov/api/species/${species}`
   );
   console.log("Request sent to API");
   return apiResponse.data;
 }
+
+async function getSpeciesData(req, res) {
+  const species = req.params.species;
+  let results;
+
+  try {
+    results = await fetchApiData(species);
+
+    if (results.length === 0) {
+      throw "API returned empty array";
+    }
+    res.send({
+      fromCache: false,
+      data: results,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(404).send("Data unavailable");
+  }
+}
+
+app.get("/fish/:species", getSpeciesData);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
